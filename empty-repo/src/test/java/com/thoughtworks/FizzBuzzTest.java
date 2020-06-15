a6baf9d0791f19c7c3570e08150b02964d4a9a7c
kekeing <851575436@qq.com>
2020-06-15 14:39:17 +0800

    add by like -finish should_get_number_1_when_given_1

--- empty-repo/src/test/java/com/thoughtworks/FizzBuzzTest.java
+++ empty-repo/src/test/java/com/thoughtworks/FizzBuzzTest.java
@@ -2,18 +2,21 @@ package com.thoughtworks;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class FizzBuzzTest {
 
     @Test
-    public void should_get_number_1_when_given_1()
-    {
+    public void should_get_number_1_when_given_1() {
         //given
         FizzBuzz fizzBuzz = new FizzBuzz();
 
-        //when
 
+        //when
+        String result = fizzBuzz.getResult(1);
 
         //then
+        assertEquals(result, "1");
     }
 
 }
