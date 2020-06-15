7f6b5ba3a6636809571cbbf1ff4024beba150b4d
kekeing <851575436@qq.com>
2020-06-15 15:25:35 +0800

    add by like refactor add resultString

--- empty-repo/src/main/java/com.thoughtworks/FizzBuzz.java
+++ empty-repo/src/main/java/com.thoughtworks/FizzBuzz.java
@@ -2,40 +2,28 @@ package com.thoughtworks;
 
 public class FizzBuzz {
 
-    public boolean isDevidedByDivided(int number,int divisor){
+    public boolean isDividedByDivided(int number,int divisor){
         if (number % divisor ==0){return true;}
         return false;
     }
     public String getResult(int number){
         int FizzNumber = 3;
         int BuzzNumber = 5;
-        int FizzBuzzNumber = 15;
         int WhizzNumber = 7;
-        int FizzWhizzNumber = 21;
-        int BuzzWhizzNumber = 35;
-        int FizzBuzzWhizzNumber = 105;
-
-        if (isDevidedByDivided(number,FizzBuzzWhizzNumber)){
-            return "FizzBuzzWhizz";
+        String resultString = "";
+         if (isDividedByDivided(number,FizzNumber)){
+            resultString = resultString + "Fizz";
         }
-        else if (isDevidedByDivided(number,BuzzWhizzNumber)){
-            return "BuzzWhizz";
+         if (isDividedByDivided(number,BuzzNumber)) {
+             resultString = resultString + "Buzz";
         }
-        else if (isDevidedByDivided(number,FizzWhizzNumber)){
-            return "FizzWhizz";
+         if (isDividedByDivided(number,WhizzNumber)) {
+             resultString = resultString + "Whizz";
         }
-        else if (isDevidedByDivided(number,FizzBuzzNumber)){
-            return "FizzBuzz";
-        }
-        else if (isDevidedByDivided(number,FizzNumber)){
-            return "Fizz";
-        } else if (isDevidedByDivided(number,BuzzNumber)) {
-        return "Buzz";
-        }
-        else if (isDevidedByDivided(number,WhizzNumber)) {
-            return "Whizz";
-        }
-
-        return String.valueOf(number);
+       if (resultString == ""){
+           return String.valueOf(number);
+       }else {
+           return resultString;
+       }
     }
 }
