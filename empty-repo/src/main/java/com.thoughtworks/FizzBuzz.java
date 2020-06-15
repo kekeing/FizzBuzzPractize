7c27baaa610326a82155fb8a0927c84d87ba18c3
kekeing <851575436@qq.com>
2020-06-15 14:43:47 +0800

    add by like finish should_get_number_1_when_given_1

--- empty-repo/src/main/java/com.thoughtworks/FizzBuzz.java
+++ empty-repo/src/main/java/com.thoughtworks/FizzBuzz.java
@@ -1,8 +1,16 @@
 package com.thoughtworks;
 
 public class FizzBuzz {
+    public boolean isDevidedBy3(int number){
+        if (number % 3 ==0){return true;}
+        return false;
+
+    }
 
     public String getResult(int number){
-        return null;
+        if (isDevidedBy3(number)){
+            return "Fizz";
+        }
+        return String.valueOf(number);
     }
 }
